f0cddb5b972186b07211cdc07b155bb1cc3c2afa
Mike Alves <michelalvs@gmail.com>
2022-08-11 20:08:28 -0300

    Implemented a quit button and changed default window color

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-print("Hello world")
+from views import Colors
+
+import tkinter as tk
+from tkinter import Button, ttk
+from tkinter import messagebox
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # default window size
+        window_width = 800
+        window_height = 600
+
+        # Get system current screen size
+        screen_width = self.winfo_screenwidth
+        screen_height = self.winfo_screenheight
+
+        # find the center point
+        # center_x = int(screen_width / 2 - window_width / 2)
+        # center_y = int(screen_height / 2 - window_height / 2)
+
+        self.geometry(f'{window_width}x{window_height}')#{center_x}+{center_y}')
+
+        self.title('FacilitaIR')
+        self.configure(background=Colors.c0_powder_blue)
+
+        button_quit = Button(self, command=quit, text="Fechar", width=8)
+        button_quit.place(x=300, y=500)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
---
+++ views/Colors.py
@@ -0,0 +1,11 @@
+c0_powder_blue = "#C3DFE0"  # Powder Blue
+c1_yellow_green_crayola = "#BCD979"  # Yellow Green Crayola
+c2 = "#9DAD6F"  # Olivine
+c3 = "#7D6D61"  # French Bistre
+c4 = "#5E574D"   # Davis Grey
+
+c5 = "#e06636"   # - profit
+c6 = "#038cfc"   # azul
+c7 = "#ef5350"   # vermelha
+c8 = "#263238"   # + verde
+c9 = "#e9edf5"   # sky blue
